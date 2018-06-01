acc047c8e902a80f882e18939cc8443acbc16551
serenalai <serena.lai@honestbee.com>
2018-06-01 10:53:41 +0800

    dev: todo list - can create data only

--- App.js
+++ App.js
@@ -1,14 +1,126 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/* @flow
+ */
 
-export default class App extends React.Component {
+import React, {
+  Component
+} from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  Button,
+  TouchableHighlight,
+  Alert
+} from 'react-native';
+
+export default class FlatListBasics extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [{
+        key: 'apple'
+      }, {
+        key: 'banana'
+      }, {
+        key: 'cat'
+      }, {
+        key: ''
+      }],
+      text: '',
+      count: 4,
+    }
+  }
+
+  _handlePress() {
+    var count = this.state.count + 1;
+    console.debug('count =' + count);
+    console.debug('The text input is:' + this.state.text);
+    const item = {
+      key: this.state.text
+    }
+    const data = this.state.data.concat(item);
+    // this.state.data.push(item);
+    this.setState({
+      // data: [...this.state.data, item]
+      data
+    })
+    this.setState({
+      count
+    });
+  }
+  _handleTextChanged(text) {
+    console.debug('_handleTextChanged');
+  }
+
+  _removeItem(index) {
+    console.debug('remove:' + index);
+    // const data = this.state.data;
+    // const result = data.filter(this.state.data[index])
+    // const data = this.state.data.splice(index, 1);
+    // console.debug('result:' + data);
+    // this.setState({
+    //  data: data
+    // })
+  }
+
+  _pressBtn = (index) => {
+    console.debug(index);
+    Alert.alert('Touched')
+  }
+
+  _renderItem = ({
+    item,
+    index
+  }) => {
+    // console.debug(item.count)
+    // onPress={()=> {console.debug(index)}}
+    return (
+      <TouchableHighlight
+      onPress={()=> {
+        console.debug(index)
+        this._removeItem(index)
+      }
+      }
+      underlayColor='black'
+    >
+      <Text>{item.key}</Text>
+    </TouchableHighlight>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <View style={styles.header}> <Text> My TODO List</Text> </View>
+          <FlatList style={styles.list}
+            data={(this.state.data)}
+        extraData = {this.state}
+            // renderItem={this._renderItem}
+          renderItem={this._renderItem}
+        onRefresh={this._onRefresh}
+          />
+        <TextInput style={styles.inputField}
+            // onChangeText={ (text) => this._handleTextChanged(text)} 
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+          />
+          <Button
+            style={styles.button}
+        onPress={() => {
+        // console.debug('Pressed!!!!');
+        console.log('data='+ this.state.data)
+        // this.state.data.push('kanewnew')
+        console.log(this.state.data);
+        this._handlePress()
+        }}
+        title="Add a event"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+        </View>
     );
   }
 }
@@ -16,8 +128,37 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 22,
   },
-});
+  header: {
+    // flex: 1,
+    paddingTop: 22,
+    height: 50,
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  list: {
+    marginBottom: 20,
+    height: 200,
+    // paddingBottom: 10,
+    backgroundColor: 'rgba(255,200,200,1.0)',
+  },
+  inputField: {
+    flex: 2,
+    marginLeft: 20,
+    marginRight: 20,
+    // marginBottom: 60,
+    // paddingBottom: 40,
+    height: 40,
+    backgroundColor: 'rgba(0,200,200,1.0)',
+  },
+  button: {
+    flex: 3,
+    marginBottom: 30,
+    height: 40,
+  }
+})
